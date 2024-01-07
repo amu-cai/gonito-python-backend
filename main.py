ab9f780375bc44e8b3492d34782ef64614d27170
Mateusz Tylka <mattyl@st.amu.edu.pl>
2024-01-07 20:22:54 +0100

    implementation and empty challenges and temp

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import models
 from typing import Annotated
-from fastapi import Depends, FastAPI, status, HTTPException
+from fastapi import Depends, FastAPI, status, HTTPException, UploadFile, File
 from database import engine, SessionLocal
+from secrets import token_hex
 from sqlalchemy.orm import Session
 import auth
 from auth import get_current_user
+import zipfile
+import os
+from glob import glob
 
 app = FastAPI()
 app.include_router(auth.router)
@@ -26,3 +30,23 @@ async def user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
     return {"User": user}
+
+@app.post("/create-challenge")
+async def create_challenge(file:UploadFile = File(...)):
+    file_ext = file.filename.split(".").pop()
+    if file_ext != "zip":
+        raise HTTPException(status_code=401, detail='Bad extension')
+    challenges_dir = "./challenges"
+    file_name = token_hex(10)
+    file_path = f"{file_name}.{file_ext}"
+    with open(f"temp/{file_path}", "wb") as f:
+        content = await file.read()
+        f.write(content)
+    with zipfile.ZipFile(f"temp/{file_path}", 'r') as zip_ref:
+        current_challenges = [x.replace(f"{challenges_dir}\\", '') for x in glob(f"{challenges_dir}/*")]
+        if zip_ref.filelist[0].filename[:-1] in current_challenges:
+            raise HTTPException(status_code=401, detail='Challenge has been already created!')
+        zip_ref.extractall(challenges_dir)
+    os.remove(f"temp/{file_path}")
+    # TODO: Sprawdzanie struktury challenge'a, tylko admin może tworzyć challenge, refactor
+    return {"success": True, "file_path": file_path, "message": "File uloaded successfully"}
